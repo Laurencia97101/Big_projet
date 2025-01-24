723e31f55ddce307b86b65d8c855bde6fd0b2671
Laurencia97101 <clamylaurencia@hotmail.com>
2025-01-24 15:07:03 +0100

    mes sélection sont faits

--- asset/js/essaie.js
+++ asset/js/essaie.js
@@ -131,63 +131,81 @@
 
 
 /* script.js */
+// à prendre
+// document.addEventListener("DOMContentLoaded", function() {
+//     const canvas = document.createElement('canvas');
+//     document.body.appendChild(canvas);
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+//     const ctx = canvas.getContext('2d');
 
-document.addEventListener("DOMContentLoaded", function() {
-    const canvas = document.createElement('canvas');
-    document.body.appendChild(canvas);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const ctx = canvas.getContext('2d');
+//     const particlesArray = [];
 
-    const particlesArray = [];
+//     class Particle {
+//         constructor() {
+//             this.x = Math.random() * canvas.width;
+//             this.y = Math.random() * canvas.height;
+//             this.size = Math.random() * 3 + 1;
+//             this.speedX = Math.random() * 0.5 - 0.25;
+//             this.speedY = Math.random() * 0.5 - 0.25;
+//         }
+//         update() {
+//             this.x += this.speedX;
+//             this.y += this.speedY;
 
-    class Particle {
-        constructor() {
-            this.x = Math.random() * canvas.width;
-            this.y = Math.random() * canvas.height;
-            this.size = Math.random() * 3 + 1;
-            this.speedX = Math.random() * 0.5 - 0.25;
-            this.speedY = Math.random() * 0.5 - 0.25;
+//             // Replacer les particules qui sortent des bords
+//             if (this.x > canvas.width) this.x = 0;
+//             if (this.x < 0) this.x = canvas.width;
+//             if (this.y > canvas.height) this.y = 0;
+//             if (this.y < 0) this.y = canvas.height;
+//         }
+//         draw() {
+//             ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+//             ctx.beginPath();
+//             ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+//             ctx.closePath();
+//             ctx.fill();
+//         }
+//     }
+
+//     function createParticles() {
+//         for (let i = 0; i < 100; i++) {
+//             particlesArray.push(new Particle());
+//         }
+//     }
+
+//     function handleParticles() {
+//         for (let i = 0; i < particlesArray.length; i++) {
+//             particlesArray[i].update();
+//             particlesArray[i].draw();
+//         }
+//     }
+
+//     function animate() {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//         handleParticles();
+//         requestAnimationFrame(animate);
+//     }
+
+//     createParticles();
+//     animate();
+// });
+
+// script.js
+// script.js
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const faqAnswer = button.nextElementSibling;
+
+        button.classList.toggle('active');
+        faqAnswer.classList.toggle('open');
+
+        if (faqAnswer.style.maxHeight) {
+            faqAnswer.style.maxHeight = null;
+        } else {
+            faqAnswer.style.maxHeight = faqAnswer.scrollHeight + "px";
         }
-        update() {
-            this.x += this.speedX;
-            this.y += this.speedY;
-
-            // Replacer les particules qui sortent des bords
-            if (this.x > canvas.width) this.x = 0;
-            if (this.x < 0) this.x = canvas.width;
-            if (this.y > canvas.height) this.y = 0;
-            if (this.y < 0) this.y = canvas.height;
-        }
-        draw() {
-            ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-            ctx.closePath();
-            ctx.fill();
-        }
-    }
-
-    function createParticles() {
-        for (let i = 0; i < 100; i++) {
-            particlesArray.push(new Particle());
-        }
-    }
-
-    function handleParticles() {
-        for (let i = 0; i < particlesArray.length; i++) {
-            particlesArray[i].update();
-            particlesArray[i].draw();
-        }
-    }
-
-    function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        handleParticles();
-        requestAnimationFrame(animate);
-    }
-
-    createParticles();
-    animate();
+    });
 });
 
--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,17 @@
 // script.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
 
 
 exportdocument.addEventListener("DOMContentLoaded", function() {
